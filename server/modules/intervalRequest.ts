b1654b152599680c6fcae89fd244c9eb0ad9a543
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2021-11-10 20:28:15 +0900

    インターバル確認

--- server/modules/intervalRequest.ts
+++ server/modules/intervalRequest.ts
@@ -1,24 +1,26 @@
 import axios from "axios";
 
 export const intervalRequest = () => {
-  const currentHour = new Date().getHours();
-  const currentMinute = new Date().getMinutes();
-  let interval: NodeJS.Timer | null = null;
+  // const currentHour = new Date().getHours();
+  // const currentMinute = new Date().getMinutes();
+  // let interval: NodeJS.Timer | null = null;
 
-  const request = () => {
+  const request = async () => {
     return axios.get("https://heroku-tutorial-2021.herokuapp.com/");
   };
 
-  // 毎朝7時からインターバルスタート
-  setTimeout(() => {
-    // herokuの仕様で30分リクエストがないとスリープしてしまうため30分ごとにリクエスト送信
-    interval = setInterval(request, 60000 * 30);
-  }, new Date().setHours(7, 0, 0, 0) - new Date().setHours(currentHour, currentMinute));
+  setInterval(request, 60000 * 30);
 
-  // 毎夜23:59でインターバル終了
-  setTimeout(() => {
-    if (interval) {
-      clearInterval(interval);
-    }
-  }, new Date().setHours(23, 59, 0, 0) - new Date().setHours(currentHour, currentMinute));
+  // 毎朝7時からインターバルスタート
+  // setTimeout(() => {
+  //   // herokuの仕様で30分リクエストがないとスリープしてしまうため30分ごとにリクエスト送信
+  //   interval = setInterval(request, 60000 * 30);
+  // }, new Date().setHours(7, 0, 0, 0) - new Date().setHours(currentHour, currentMinute));
+
+  // // 毎夜23:59でインターバル終了
+  // setTimeout(() => {
+  //   if (interval) {
+  //     clearInterval(interval);
+  //   }
+  // }, new Date().setHours(23, 59, 0, 0) - new Date().setHours(currentHour, currentMinute));
 };
